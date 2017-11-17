61121ffaae928b04aa4c106d9a1b67aae4d5128e
Wiktor Kielar <wiktor.kielar@euvic.pl>
2017-11-17 16:02:06 +0100

    Added swagger and started configuration (not annotated properly yet)

--- server/movies-mananger/src/main/java/com/wiktorkielar/moviesmananger/model/Movie.java
+++ server/movies-mananger/src/main/java/com/wiktorkielar/moviesmananger/model/Movie.java
@@ -4,13 +4,33 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-public class Movie {
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
+@ApiModel
+public class Movie {
+	
+	
+	
+
+	@ApiModelProperty(
+			hidden = true)
 	private String id;
+//	@ApiModelProperty(
+//			required=true, 
+//			notes =  
+//			TITLE + SPACE + CANNOT_BE_EMPTY + SPACE +
+//			TITLE + SPACE + CANNOT_BE_MISSING + SPACE + 
+//			TITLE + MUST_CONTAIN_MIN + SPACE + "3"
+// 			cannot be empty. and has to contain at least 3 characters but no more than 50 characters")
 	private String title;
+	@ApiModelProperty(required=true)
 	private Double rating;
+	@ApiModelProperty(required=true)
 	private String director;
+	@ApiModelProperty(required=true)
 	private List<String> actors;
+	@ApiModelProperty(hidden = true)
 	private LocalDateTime createdAt;
 
 	public Movie(String title, Double rating, String director, List<String> actors) {
